b69afcfadfa6bd4266eb536f47c49fefd12125bc
Luc2000 <lucassouzaannunziato@gmail.com>
2026-09-01 19:22:44 -0300

    feat: add devs section to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import Hero from "./components/Hero";
+import DevsSection from "./components/DevsSection";
 import Services from "./components/Services";
 import FeaturedProjects from "./components/FeaturedProjects";
 import WhyChooseUs from "./components/WhyChooseUs";
 import ContactCTA from "./components/ContactCTA";
 
+export const revalidate = 3600;
+
 export default function Home() {
   return (
     <>
       <Hero />
+      <DevsSection source="home" />
       <Services />
       <FeaturedProjects />
       <WhyChooseUs />
